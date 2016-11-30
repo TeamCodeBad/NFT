08a887afbbf723288fbd5eed5e13bea6af2ff1a9
Kevin Liu <krliu@cpp.edu>
2016-11-29 21:10:30 -0800

    Check Sum

--- NFTT/src/main/CheckSum.java
+++ NFTT/src/main/CheckSum.java
@@ -10,7 +10,8 @@ public class CheckSum {
 	public CheckSum(File filename) {
 		this.array = convertFile(filename);
 	}
-
+	public CheckSum(){
+	}
 	private byte[] convertFile(File filename) {
 
 		byte[] bFile = new byte[(int) filename.length()];
@@ -31,7 +32,90 @@ public class CheckSum {
 		}
 		return bFile;
 	}
+	public String checkSum(File f){
+		// ArrayList<Byte> sentinel = new ArrayList<Byte>();
+		byte[] array = convertFile(f);
+		String result = "";
+		int count = 0;
 
+		BigInteger piece1;
+		BigInteger piece2;
+		BigInteger temp = new BigInteger("0");
+		String order = (new BigInteger(array)).toString(8);
+		// 0 1 2 3 4 5 6 7
+		// v - x * + / ^ !
+		long factor = 1000;
+		for (int i = 0; i < array.length; i++) {
+			// System.out.println(order.charAt(count % 8));
+			switch (order.charAt(count % 8)) {
+			case '0':
+				piece1 = new BigInteger("" + (long) Math.abs((Math.cos(array[i]) * 5000 * factor)));
+				piece2 = new BigInteger("" + (long) Math.abs((Math.sin(array[i]) * 5000 * factor)));
+				piece1 = piece1.xor(piece2);
+
+				temp = temp.add(piece1);
+				break;
+			case '1':
+				piece1 = new BigInteger("" + ((long) (Math.pow((array[i]), 5) * 2 * factor)));
+				piece2 = new BigInteger("" + (long) Math.abs((Math.sin(array[i]) * 5000 * factor)));
+				piece1 = piece1.or(piece2);
+
+				temp = temp.add(piece1.shiftLeft(10));
+				break;
+			case '2':
+				piece1 = new BigInteger("" + (long) Math.abs((Math.cos(array[i]) * 5000 * factor)));
+				piece2 = new BigInteger("" + (long) Math.abs((factorial(array[i]) * 6 * factor)));
+				piece1 = piece1.add(piece2);
+
+				temp = temp.add(piece1);
+				break;
+			case '3':
+				piece1 = new BigInteger("" + (long) Math.abs((fibonacci(array[i]) * 1469 * factor)));
+				piece2 = new BigInteger("" + (long) Math.abs((Math.sin(array[i]) * 5000 * factor)));
+				piece1 = piece1.and((piece2));
+
+				temp = temp.add(piece1);
+				break;
+			case '4':
+				piece1 = new BigInteger("" + (long) Math.abs((Math.cosh(array[i]) * 55 * factor)));
+				piece2 = new BigInteger("" + (long) Math.abs((Math.sinh(array[i]) * 55 * factor)));
+				piece1 = piece1.andNot(piece2);
+
+				temp = temp.add(piece1);
+				break;
+			case '5':
+				piece1 = new BigInteger("" + (long) Math.abs((Math.cosh(array[i]) * 5000 * factor)));
+				piece2 = new BigInteger("" + (long) Math.abs((Math.sin(array[i]) * 55 * factor)));
+				piece1 = piece1.or(piece2);
+
+				temp = temp.add(piece1);
+				break;
+			case '6':
+				piece1 = new BigInteger("" + (long) Math.abs((Math.cos(array[i]) * 5000 * factor)));
+				piece2 = new BigInteger("" + (long) Math.abs((Math.sinh(array[i]) * 55 * factor)));
+				piece1 = piece1.subtract(piece2);
+
+				temp = temp.add(piece1);
+				break;
+			case '7':
+				piece1 = new BigInteger("" + (long) Math.abs((Math.cos(array[i]) * 5000 * factor)));
+				piece2 = new BigInteger("" + (long) Math.abs((Math.sin(array[i]) * 5000 * factor)));
+				piece1 = piece1.add(piece2);
+
+				temp = temp.subtract(piece1);
+				break;
+			default:
+				break;
+			}
+
+			count++;
+		}
+
+		temp = temp.mod(new BigInteger("1298074214633706835075030044377087"));
+
+		result = temp.toString(16);
+		return result;
+	}
 	public String checkSum() {
 		// ArrayList<Byte> sentinel = new ArrayList<Byte>();
 		String result = "";
